5cbdcd72de79aeaae9481b520d9f81948e4f51a9
iury <satriani-16@hotmail.com>
2015-07-06 02:53:05 -0300

    :heart:

--- backend/appengine/static/jogos/jogo_form.js
+++ backend/appengine/static/jogos/jogo_form.js
@@ -7,16 +7,26 @@ jogoModulo.directive('jogoform', function(){
         templateUrl: '/static/jogos/novo.html',
         scope:{
             game: '=',
+            isEditing: "=?",
+            editOrCreate: "=?",
             formOnOff: "&"
         },
         controller:function($scope, $http){
+           var save_or_edit_url =  '/jogos/rest/new';
             $scope.salvar = function(){
-                $http.post('/jogos/rest/new', $scope.game).success(function(game){
-                    console.log(game);
-                }).error(function(errors){
-                    console.log(errors);
-                });
-            };
+              if ($scope.isEditing){
+                save_or_edit_url = '/jogos/rest/edit';
+                $scope.game.jogo_id = $scope.game.id;
+              }
+              else{
+                save_or_edit_url = '/jogos/rest/new';
+              }
+              $http.post(save_or_edit_url, $scope.game).success(function(game){
+                  console.log(game);
+              }).error(function(errors){
+                  console.log(errors);
+              });
+          };
         }
     };
 });
