ee68328fba3e34f72aa18ec13332353d13477bc9
iury <satriani-16@hotmail.com>
2015-07-07 03:56:14 -0300

    :thunder:

--- backend/appengine/static/jogos/jogo_form.js
+++ backend/appengine/static/jogos/jogo_form.js
@@ -23,7 +23,6 @@ jogoModulo.directive('jogoform', function(){
               }
               else{
                 save_or_edit_url = '/jogos/rest/new';
-                  $scope.games.push($scope.game);
               }
               $http.post(save_or_edit_url, $scope.game).success(function(game){
                   console.log(game);
@@ -34,6 +33,7 @@ jogoModulo.directive('jogoform', function(){
                   $scope.game.grp='';
                   $scope.salvando = false
                   $scope.formOnOff();
+                  $scope.games.push($scope.game);
               }).error(function(errors){
                   $scope.errors=errors;
                   console.log(errors);
