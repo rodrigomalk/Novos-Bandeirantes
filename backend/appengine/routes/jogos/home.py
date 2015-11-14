5e22a8806c05c22f29799b80b7cd5af308dbea1b
iury <satriani-16@hotmail.com>
2015-11-13 22:45:35 -0200

    cara, ta muito confuso tudo isso

--- backend/appengine/routes/jogos/home.py
+++ backend/appengine/routes/jogos/home.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import absolute_import, unicode_literals
+import json
 from config.template_middleware import TemplateResponse
 from gaepermission.decorator import login_not_required
 from tekton import router
@@ -15,22 +17,16 @@ def index():
     cmd = jogo_facade.list_jogos_cmd()
     jogos = cmd()
     edit_path = router.to_path(edit)
-    delete_path = router.to_path(delete)
     jogo_form = jogo_facade.jogo_form()
 
     def localize_jogo(jogo):
         jogo_dct = jogo_form.fill_with_model(jogo)
         jogo_dct['edit_path'] = router.to_path(edit_path, jogo_dct['id'])
-        jogo_dct['delete_path'] = router.to_path(delete_path, jogo_dct['id'])
         return jogo_dct
 
     localized_jogos = [localize_jogo(jogo) for jogo in jogos]
     context = {'jogos': localized_jogos,
                'new_path': router.to_path(new)}
-    return TemplateResponse(context, 'jogos/jogo_home.html')
+    return TemplateResponse(context, 'jogos/home.html')
 
-@login_not_required
-def delete(jogo_id):
-    jogo_facade.delete_jogo_cmd(jogo_id)()
-    return RedirectResponse(router.to_path(index))
 
