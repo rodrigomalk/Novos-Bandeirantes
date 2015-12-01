c784a26598de1c584b530704e63b1eeb00ace5de
rodrigomalk <contato@rodrigomalk.com.br>
2015-12-01 01:01:39 -0200

    Tentativa de mostrar medalhas

--- backend/appengine/routes/jogos/home.py
+++ backend/appengine/routes/jogos/home.py
@@ -6,13 +6,25 @@ from forms import GameFormTable
 from google.appengine.ext import ndb
 from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
+from models import Result
 
 @no_csrf
-def index():
+def index(_logged_user):
+    user_key = _logged_user.key
+    query = Result.query(Result.user == user_key)
+    results = query.fetch()
+    resp = "---"
+    medal = False
     query = Autor.query()
     autores = query.fetch()
     game_keys = [autor.destination for autor in autores]
     jogo_lista = ndb.get_multi(game_keys)
+    for game in jogo_lista:
+        for result in results:
+            if game.key == result.game:
+                if result.won_medal is True:
+                    medal = True
+                resp = "%d / %d" % (result.best_points, result.size)
     form = GameFormTable()
     jogo_lista = [form.fill_with_model(jogo) for jogo in jogo_lista]
-    return TemplateResponse({"games": jogo_lista}, 'jogos/home.html')
+    return TemplateResponse({"games": jogo_lista, "resp": resp, "medal": medal}, 'jogos/home.html')
